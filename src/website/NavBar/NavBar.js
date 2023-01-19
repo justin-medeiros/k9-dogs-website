0852e92691bb11b57a89a494185ff73d3a0ba2af
jmedeiros11 <jmedeiros1011@gmail.com>
2023-01-19 14:34:11 -0500

    Created navabr and implemented mobile version

--- src/website/NavBar/NavBar.js
+++ src/website/NavBar/NavBar.js
@@ -4,11 +4,30 @@ import "./NavBar.css";
 
 export default function NavBar() {
   const [currentTab, setCurrentTab] = useState("");
+  const [navButton, setNavButton] = useState(false);
+  const [menuIconClick, setMenuIconClick] = useState(false);
 
   useEffect(() => {
     setCurrentTab(window.location.pathname);
     console.log(currentTab);
+    console.log(navButton);
   }, [window.location.pathname]);
+
+  const onMenuIconClick = () => {
+    setMenuIconClick((prevClick) => {
+      return !prevClick;
+    });
+  };
+
+  const showNavButton = () => {
+    if (window.innerWidth <= 960) {
+      setNavButton(true);
+    } else {
+      setNavButton(false);
+    }
+  };
+
+  window.addEventListener("resize", showNavButton);
 
   return (
     <nav className="navbar">
@@ -16,47 +35,85 @@ export default function NavBar() {
         <NavLink to="/">
           <img className="navbar--logo" src="/images/logo-w-r-v2.png"></img>
         </NavLink>
-        <ul>
+        <ul
+          className={menuIconClick ? "navbar--items active" : "navbar--items"}
+        >
           <li>
-            <NavLink to="/" className="navbar--links">
+            <NavLink
+              to="/"
+              className={
+                menuIconClick ? "navbar--mobile--links" : "navbar--links"
+              }
+            >
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/ourdogs" className="navbar--links">
+            <NavLink
+              to="/ourdogs"
+              className={
+                menuIconClick ? "navbar--mobile--links" : "navbar--links"
+              }
+            >
               Our Dogs
             </NavLink>
           </li>
           <li>
-            <NavLink to="/puppies" className="navbar--links">
+            <NavLink
+              to="/puppies"
+              className={
+                menuIconClick ? "navbar--mobile--links" : "navbar--links"
+              }
+            >
               Puppies
             </NavLink>
           </li>
           <li>
-            <NavLink to="/gallery" className="navbar--links">
+            <NavLink
+              to="/gallery"
+              className={
+                menuIconClick ? "navbar--mobile--links" : "navbar--links"
+              }
+            >
               Gallery
             </NavLink>
           </li>
           <li>
-            <NavLink to="/testimonials" className="navbar--links">
+            <NavLink
+              to="/testimonials"
+              className={
+                menuIconClick ? "navbar--mobile--links" : "navbar--links"
+              }
+            >
               Testimonials
             </NavLink>
           </li>
           <li>
-            <NavLink to="/faq" className="navbar--links">
+            <NavLink
+              to="/faq"
+              className={
+                menuIconClick ? "navbar--mobile--links" : "navbar--links"
+              }
+            >
               FAQ
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/contact"
-              id={currentTab === "/contact" ? "active" : ""}
-              className="navbar--links"
+              className={
+                menuIconClick ? "navbar--mobile--links" : "navbar--links"
+              }
             >
               Contact Us
             </NavLink>
           </li>
         </ul>
+        <div className="menu--icon" onClick={onMenuIconClick}>
+          <i
+            className={menuIconClick ? "fa-solid fa-xmark" : "fa-solid fa-bars"}
+          ></i>
+        </div>
       </div>
     </nav>
   );

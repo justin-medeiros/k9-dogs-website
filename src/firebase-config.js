e308255b3298eb27ab799722e5a26870a75d45c7
jmedeiros11 <jmedeiros1011@gmail.com>
2023-02-16 18:36:53 -0500

    added about component on home page

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -19,6 +19,8 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
+//Change to dotenv
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
--- src/website/Home/Home.js
+++ src/website/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "./components/About";
 import HeroSection from "./components/HeroSection";
 import "./Home.css";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <div>
       <HeroSection />
+      <About />
     </div>
   );
 }

ba77e10a4bc5acd78cdf5d4f9fc70ba584045b57
jmedeiros11 <jmedeiros1011@gmail.com>
2023-02-17 14:49:23 -0500

    Changed colors to css variables

---
+++ src/website/Home/items/DogCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DogCard() {
+  return (
+    <div className="dogcard--container">
+      <h1 className="dogcard--title">Name</h1>
+      <img src="/images/test.png"></img>
+      <h2>Just a test run</h2>
+    </div>
+  );
+}

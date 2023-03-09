4bc6cb109469c9b78635914c5981998750de1621
jmedeiros11 <jmedeiros1011@gmail.com>
2023-03-09 17:54:04 -0500

    FAQ Page complete

--- src/website/FAQ/FAQ.jsx
+++ src/website/FAQ/FAQ.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./FAQ.css";
 import FAQCard from "./items/FAQCard";
+import data from "../../data.json";
+import { Link } from "react-router-dom";
 
 export default function FAQ() {
+  const [allFAQ, setAllFAQ] = useState([]);
+
+  useEffect(() => {
+    const faqCards = data.faq.map((data) => {
+      return <FAQCard key={data.key} data={data} />;
+    });
+    setAllFAQ(faqCards);
+  }, []);
+
   return (
     <div className="faq--container">
       <h1 className="faq--title">Frequently Asked Questions</h1>
-      <FAQCard />
+      {allFAQ}
+      <p className="faq--contact">
+        Can't find answers here? Feel free to{" "}
+        <span>
+          <Link to="/contact" className="faq--contact--us">
+            Contact Us
+          </Link>
+        </span>
+        .
+      </p>
     </div>
   );
 }

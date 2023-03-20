406c28cde1a1365643b5bc08c5d724ed1f1f9ff9
jmedeiros11 <jmedeiros1011@gmail.com>
2023-03-20 18:45:33 -0400

    Added custom buttons to slider

--- src/website/Litters/items/PastLittersCard.jsx
+++ src/website/Litters/items/PastLittersCard.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 import Slider from "react-slick";
 import "./PastLittersCard.css";
 
 export default function PastLittersCard() {
+  const sliderRef = useRef(null);
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows: false,
+  };
+
+  const handleNextClick = () => {
+    sliderRef.current.slickNext(); // Call slickNext() method to go to the next slide
+  };
+
+  const handlePrevClick = () => {
+    sliderRef.current.slickPrev(); // Call slickNext() method to go to the next slide
   };
 
   return (
     <div className="litters--card--container">
       <div className="litters--card--slider--container">
-        <Slider {...settings}>
+        <div className="slider--buttons--container">
+          <div className="prev--button" onClick={handlePrevClick}>
+            <i className="fa-solid fa-chevron-left"></i>
+          </div>
+          <div className="next--button" onClick={handleNextClick}>
+            <i className="fa-solid fa-chevron-right"></i>
+          </div>
+        </div>
+
+        <Slider {...settings} ref={sliderRef}>
           <div className="litters--card--image--container">
             <img
               className="litters--card--image"

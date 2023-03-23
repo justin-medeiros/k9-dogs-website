f075567540dd0b1b2f016d16baf4ed6b08f20ec8
jmedeiros11 <jmedeiros1011@gmail.com>
2023-03-23 00:03:46 -0400

    Our Dogs page finsihed setup

--- src/website/OurDogs/OurDogs.jsx
+++ src/website/OurDogs/OurDogs.jsx
@@ -5,9 +5,16 @@ import "./OurDogs.css";
 export default function OurDogs() {
   return (
     <div className="ourdogs--container">
-      <h1 className="ourdogs--title">Our Dogs</h1>
-      <h3 className="ourdogs--subtitle">Meet the stars of our kennel</h3>
+      <div className="ourdogs--top--container">
+        <div className="ourdogs--title--container">
+          <h1 className="ourdogs--title">Our Dogs</h1>
+        </div>
+        <h3 className="ourdogs--subtitle">Meet the stars of our kennel!</h3>
+      </div>
       <div className="ourdogs--dogs--container">
+        <OurDogsCard />
+        <OurDogsCard />
+        <OurDogsCard />
         <OurDogsCard />
       </div>
     </div>
--- src/website/OurDogs/items/OurDogsCard.jsx
+++ src/website/OurDogs/items/OurDogsCard.jsx
@@ -3,15 +3,45 @@ import "./OurDogsCard.css";
 
 export default function OurDogsCard() {
   return (
-    <div className="ourdogs--card--container">
-      <div className="ourdogs--card--image--container">
-        <img className="ourdogs--card--image" src="/images/image-gs.jpeg"></img>
-      </div>
+    <div className="ourdogs--card--overall--container">
+      <div className="ourdogs--card--container">
+        <div className="ourdogs--card--image--container">
+          <img
+            className="ourdogs--card--image"
+            src="/images/image-gs.jpeg"
+          ></img>
+        </div>
 
-      <div className="ourdogs--card--info">
-        <h1>Name</h1>
-        <h2>Subtitle</h2>
-        <p>About</p>
+        <div className="ourdogs--card--info">
+          <div className="ourdogs--card--info--text">
+            <div className="ourdogs--card--info--firstrow">
+              <h1 className="ourdogs--card--name">
+                V 24 (BSZS) IGP1 GEMMA VOM FRANKENGOLDEN
+              </h1>
+            </div>
+
+            <div className="ourdogs--card--info--secondrow">
+              <h2 className="ourdogs--card--dam--title">Dam</h2>
+              <h2 className="ourdogs--card--sire--title">Sire</h2>
+              <div className="ourdogs--card--sire--container"></div>
+            </div>
+
+            <div className="ourdogs--card--secondrow--text">
+              <h2>V24( BSZS) Gemma Vom Frankengold IGP 1</h2>
+              <h2>VA1 Oargos Dei Precision IGP 3 KKL</h2>
+            </div>
+
+            <div className="ourdogs--card--info--thirdrow">
+              <h2 className="ourdogs--card--description--title">Description</h2>
+            </div>
+            <h2 className="ourdogs--card--description">
+              TcjkncxczxczxjgkljgdfkljgfdklgjkldfjgkldfjgkldfgjklcjhnkzxchzxjchgfdjkjfjglkdfjgkfdjgklfdjdbvkjsdbvkjsdbvjkbdsjkvbsdkjvbsdTcjkncxczxczxjgkljgdfkljgfdklgjkldfjgkldfjgkldfgjklcjhnkzxchzxjchgfdjkjfjglkdfjgkfdjgklfd
+            </h2>
+            <button className="ourdogs--card--card--btn">
+              Click for more!
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );

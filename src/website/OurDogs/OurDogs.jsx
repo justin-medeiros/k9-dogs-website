071cfd3ce92553924bdefa99e01b6bbb8a6fb055
jmedeiros11 <jmedeiros1011@gmail.com>
2023-03-23 16:32:14 -0400

    Ourdogs page now has proper data

--- src/website/OurDogs/OurDogs.jsx
+++ src/website/OurDogs/OurDogs.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../../firebase-config";
 import OurDogsCard from "./items/OurDogsCard";
 import "./OurDogs.css";
 
 export default function OurDogs() {
+  const [ourDogs, setOurDogs] = useState([]);
+
+  useEffect(() => {
+    async function getDogs() {
+      const querySnapshot = await getDocs(collection(db, "dogs"));
+      const allDogs = querySnapshot.docs.map((doc) => {
+        const dogData = doc.data();
+        return (
+          <div key={doc.id}>
+            <OurDogsCard dogInfo={dogData} />
+          </div>
+        );
+      });
+      setOurDogs(allDogs);
+    }
+    getDogs();
+  }, []);
+
   return (
     <div className="ourdogs--container">
       <div className="ourdogs--top--container">
@@ -11,12 +31,7 @@ export default function OurDogs() {
         </div>
         <h3 className="ourdogs--subtitle">Meet the stars of our kennel!</h3>
       </div>
-      <div className="ourdogs--dogs--container">
-        <OurDogsCard />
-        <OurDogsCard />
-        <OurDogsCard />
-        <OurDogsCard />
-      </div>
+      <div className="ourdogs--dogs--container">{ourDogs}</div>
     </div>
   );
 }
--- src/website/OurDogs/items/OurDogsCard.jsx
+++ src/website/OurDogs/items/OurDogsCard.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import "./OurDogsCard.css";
 
-export default function OurDogsCard() {
+export default function OurDogsCard({ dogInfo }) {
   return (
     <div className="ourdogs--card--overall--container">
       <div className="ourdogs--card--container">
         <div className="ourdogs--card--image--container">
-          <img
-            className="ourdogs--card--image"
-            src="/images/image-gs.jpeg"
-          ></img>
+          <img className="ourdogs--card--image" src={dogInfo.img}></img>
         </div>
 
         <div className="ourdogs--card--info">
           <div className="ourdogs--card--info--text">
             <div className="ourdogs--card--info--firstrow">
-              <h1 className="ourdogs--card--name">
-                V 24 (BSZS) IGP1 GEMMA VOM FRANKENGOLDEN
-              </h1>
+              <h1 className="ourdogs--card--name">{dogInfo.name}</h1>
             </div>
 
             <div className="ourdogs--card--info--secondrow">
@@ -27,15 +22,15 @@ export default function OurDogsCard() {
             </div>
 
             <div className="ourdogs--card--secondrow--text">
-              <h2>V24( BSZS) Gemma Vom Frankengold IGP 1</h2>
-              <h2>VA1 Oargos Dei Precision IGP 3 KKL</h2>
+              <h2>{dogInfo.dam}</h2>
+              <h2>{dogInfo.sire}</h2>
             </div>
 
             <div className="ourdogs--card--info--thirdrow">
               <h2 className="ourdogs--card--description--title">Description</h2>
             </div>
             <h2 className="ourdogs--card--description">
-              TcjkncxczxczxjgkljgdfkljgfdklgjkldfjgkldfjgkldfgjklcjhnkzxchzxjchgfdjkjfjglkdfjgkfdjgklfdjdbvkjsdbvkjsdbvjkbdsjkvbsdkjvbsdTcjkncxczxczxjgkljgdfkljgfdklgjkldfjgkldfjgkldfgjklcjhnkzxchzxjchgfdjkjfjglkdfjgkfdjgklfd
+              {dogInfo.description}
             </h2>
             <button className="ourdogs--card--card--btn">
               Click for more!

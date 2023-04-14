72c7755f12d0df8c2a3b63b5bbedd76ee96f4d59
jmedeiros11 <jmedeiros1011@gmail.com>
2023-04-14 12:48:13 -0400

    Finished about section animation

--- src/website/Home/components/About.jsx
+++ src/website/Home/components/About.jsx
@@ -1,26 +1,75 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./About.css";
 import data from "../../../data.json";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 export default function About() {
+  const controlText = useAnimation();
+  const [refText, inViewText] = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  const controlImage = useAnimation();
+  const [refImage, inViewImage] = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
   const aboutText = data.about.text.split("\n");
+
+  const element = {
+    initial: { y: -20, opacity: 0 },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.7,
+        ease: [0.6, -0.05, 0.01, 0.99],
+      },
+    },
+  };
 
   const aboutTextMap = aboutText.map((item, key) => {
     return (
-      <div className="about--text" key={key}>
+      <div className="about--text">
         {item}
         {key !== aboutText.length - 1 && <br />}
       </div>
     );
   });
 
+  useEffect(() => {
+    if (inViewText) {
+      controlText.start("animate");
+    }
+
+    if (inViewImage) {
+      controlImage.start("animate");
+    }
+  }, [controlText, controlImage, inViewText, inViewImage]);
+
   return (
-    <div className="overall--container">
-      <div className="about--container">
-        <h1 className="about--title">About Us</h1>
+    <motion.div className="overall--container">
+      <motion.div
+        ref={refText}
+        variants={element}
+        initial="initial"
+        animate={controlText}
+        className="about--container"
+      >
+        <motion.h1 className="about--title">About Us</motion.h1>
         {aboutTextMap}
-      </div>
-      <img className="about--image" src="/images/about--us--image.JPEG"></img>
-    </div>
+      </motion.div>
+      <motion.img
+        ref={refImage}
+        variants={element}
+        initial="initial"
+        animate={controlImage}
+        className="about--image"
+        src="/images/about--us--image.JPEG"
+      ></motion.img>
+    </motion.div>
   );
 }

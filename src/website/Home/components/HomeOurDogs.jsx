69d99943bde6d304dff8f950441f37da5492a8b1
jmedeiros11 <jmedeiros1011@gmail.com>
2023-04-14 13:42:04 -0400

    Home animations complete

--- src/website/Home/components/HomeOurDogs.jsx
+++ src/website/Home/components/HomeOurDogs.jsx
@@ -89,19 +89,13 @@ function HomeOurDogs({ homeDogData }) {
     prevArrow: <button className="prev--button">Previous</button>,
   };
 
-  const content = {
-    animate: {
-      transition: { staggerChildren: 0.1 },
-    },
-  };
-
   const element = {
     initial: { y: -20, opacity: 0 },
     animate: {
       y: 0,
       opacity: 1,
       transition: {
-        duration: 1.0,
+        duration: 0.7,
         ease: [0.6, -0.05, 0.01, 0.99],
       },
     },
--- src/website/Home/items/HomeTestimonialCard.jsx
+++ src/website/Home/items/HomeTestimonialCard.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./HomeTestimonialCard.css";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 export default function TestimonialCard(props) {
+  const control = useAnimation();
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      control.start("animate");
+    }
+  }, [control, inView]);
+
+  const element = {
+    initial: { y: -20, opacity: 0 },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.7,
+        ease: [0.6, -0.05, 0.01, 0.99],
+      },
+    },
+  };
+
   return (
-    <div className="home--testimonial--card--container">
+    <motion.div
+      ref={ref}
+      variants={element}
+      initial="initial"
+      animate={control}
+      className="home--testimonial--card--container"
+    >
       <img
         className="home--testimonial--card--quotation"
         src="/images/quotation.png"
@@ -12,6 +44,6 @@ export default function TestimonialCard(props) {
         &rdquo;{props.info.text}&rdquo;
       </p>
       <h2 className="home--testimonial--card--author">{props.info.author}</h2>
-    </div>
+    </motion.div>
   );
 }
